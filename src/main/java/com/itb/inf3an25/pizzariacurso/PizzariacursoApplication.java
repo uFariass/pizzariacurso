abb8b38bc6459d25e9ff9cd23fd71718e430747d
Lucas Farias dos Santos <rm90326@estudante.fieb.edu.br>
2025-02-17 18:46:50 -0300

    Inicio Controle  de Versão

---
+++ src/main/java/com/itb/inf3an25/pizzariacurso/PizzariacursoApplication.java
@@ -0,0 +1,13 @@
+package com.itb.inf3an25.pizzariacurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzariacursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzariacursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itb/inf3an25/pizzariacurso/PizzariacursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.inf3an25.pizzariacurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzariacursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
